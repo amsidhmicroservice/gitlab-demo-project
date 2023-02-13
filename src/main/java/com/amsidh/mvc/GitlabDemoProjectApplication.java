2d2e7b0c63c678c189e106ffdf4ceb85fca1aa19
Amsidh Lokhande <amsidhmicroservice@gmail.com>
2023-02-13 23:28:46 +0530

    Initial commit

---
+++ src/main/java/com/amsidh/mvc/GitlabDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.amsidh.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitlabDemoProjectApplication.class, args);
+    }
+
+}
